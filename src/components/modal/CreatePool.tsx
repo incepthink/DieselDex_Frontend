a0836aaec39e7db2c08a2def8664aea4ef149f16
amankashyap004 <amankashyap004@gmail.com>
2024-12-09 18:28:30 +0530

    update: component create pool

--- src/components/modal/CreatePool.tsx
+++ src/components/modal/CreatePool.tsx
@@ -1,25 +1,27 @@
 "use client";
 
 import React, { useState } from "react";
+import Image from "next/image";
 import { IoCloseCircleOutline } from "react-icons/io5";
 import { PiQuestionDuotone } from "react-icons/pi";
 import CustomInput from "../ui/CustomInput";
 import CustomSelector from "../ui/CustomSelector";
 import { useModal } from "@/context/ModalContext";
 
-interface Token {
-  name: string;
-  symbol: string;
-  icon: string;
-}
-
-const tokens: Token[] = [
+const tokens = [
   {
-    name: "PSYCHO Coin",
-    symbol: "PSYCHO",
-    icon: "/images/icon/icon-psycho.png",
+    label: "Choose Asset",
+    value: "Choose Asset",
+    icon: "/images/icon/icon-question.png",
   },
-  { name: "Ethereum", symbol: "ETH", icon: "/images/icon/icon-eth.png" },
+  {
+    label: "ezETH",
+    value: "ezETH",
+    icon: "/images/icon/icon-pool-ezeth.png",
+  },
+  { label: "ETH", value: "ETH", icon: "/images/icon/icon-pool-eth.png" },
+  { label: "USDC", value: "USDC", icon: "/images/icon/icon-pool-usdc.png" },
+  { label: "USDT", value: "USDT", icon: "/images/icon/icon-pool-usdt.png" },
 ];
 
 const CreatePool: React.FC = () => {
@@ -31,16 +33,6 @@ const CreatePool: React.FC = () => {
   const [buyToken, setBuyToken] = useState("Choose Asset");
   const [sellAmount, setSellAmount] = useState("0");
   const [buyAmount, setBuyAmount] = useState("0");
-
-  const tokens = [
-    {
-      label: "Choose Asset",
-      value: "Choose Asset",
-      icon: "/images/icon/icon-question.png",
-    },
-    { label: "PSYCHO", value: "PSYCHO", icon: "/images/icon/icon-psycho.png" },
-    { label: "ETH", value: "ETH", icon: "/images/icon/icon-eth.png" },
-  ];
 
   return (
     <div className="fixed inset-0 flex items-center justify-center p-4 w-full bg-black/50 z-50">
@@ -57,8 +49,48 @@ const CreatePool: React.FC = () => {
           </div>
           <div className="flex flex-col gap-2 mb-4">
             <p className="font-medium">Selected Pair</p>
+
+            <div className="flex justify-between items-center w-full mb-2">
+              <div className="flex -space-x-2">
+                {sellToken !== "Choose Asset" && (
+                  <div className="w-5 lg:w-7 h-5 lg:h-7">
+                    <Image
+                      src={
+                        tokens.find((token) => token.value === sellToken)
+                          ?.icon || ""
+                      }
+                      alt="Sell token"
+                      width={100}
+                      height={100}
+                      quality={100}
+                      className="object-contain w-full"
+                    />
+                  </div>
+                )}
+                {buyToken !== "Choose Asset" && (
+                  <div className="w-5 lg:w-7 h-5 lg:h-7">
+                    <Image
+                      src={
+                        tokens.find((token) => token.value === buyToken)
+                          ?.icon || ""
+                      }
+                      alt="Buy token"
+                      width={100}
+                      height={100}
+                      quality={100}
+                      className="object-contain w-full"
+                    />
+                  </div>
+                )}
+              </div>
+              <div className="px-4 py-1 lg:py-1.5 rounded-full text-[#E16B31] bg-[#E16B31] bg-opacity-10">
+                {sellToken !== "Choose Asset" && buyToken !== "Choose Asset"
+                  ? `${sellToken}/${buyToken}`
+                  : "Select Tokens"}
+              </div>
+            </div>
             <div className="flex gap-4">
-              <div className="relative p-2 bg-[#FAF8F1] w-full rounded-md">
+              <div className="relative p-2 bg-[#FAF8F1] w-full rounded-md cursor-pointer hover:ring-1 ring-[#E16B31]">
                 <PiQuestionDuotone className="absolute top-2 right-2 text-lg text-[#757575]" />
                 <p className="font-semibold text-black text-opacity-60">
                   Volatile pool
@@ -67,7 +99,7 @@ const CreatePool: React.FC = () => {
                   0.30% fee tier
                 </p>
               </div>
-              <div className="relative p-2 bg-[#FAF8F1] w-full rounded-md">
+              <div className="relative p-2 bg-[#FAF8F1] w-full rounded-md cursor-pointer hover:ring-1 ring-[#E16B31]">
                 <PiQuestionDuotone className="absolute top-2 right-2 text-lg text-[#757575]" />
                 <p className="font-semibold text-black text-opacity-60">
                   Volatile pool
