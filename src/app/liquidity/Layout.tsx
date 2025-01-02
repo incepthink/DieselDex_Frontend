a5e5ca979ab0a2980e6753f069227b01279cb51c
incepthink <incepthink@gmail.com>
2025-01-03 01:54:24 +0530

    changes

--- src/app/liquidity/Layout.tsx
+++ src/app/liquidity/Layout.tsx
@@ -9,29 +9,29 @@ export default function LiquidityLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const router = useRouter();
-  const [isSuccess, setIsSuccess] = useState<boolean>(false);
+  // const router = useRouter();
+  // const [isSuccess, setIsSuccess] = useState<boolean>(false);
 
-  useEffect(() => {
-    (async () => {
-      const { isAuthenticated, error } = await getResponse();
+  // useEffect(() => {
+  //   (async () => {
+  //     const { isAuthenticated, error } = await getResponse();
 
-      if (error || !isAuthenticated) {
-        router.push("/login");
-        return;
-      }
+  //     if (error || !isAuthenticated) {
+  //       router.push("/login");
+  //       return;
+  //     }
 
-      setIsSuccess(true);
-    })();
-  }, [router]);
+  //     setIsSuccess(true);
+  //   })();
+  // }, [router]);
 
-  if (!isSuccess) {
-    return (
-      <main className='w-screen h-screen flex justify-center items-center bg-gradient-to-r from-[#FAF7F0] to-[#F7D4C3]'>
-        <p className='text-4xl font-semibold text-[#e16b31]'>LOADING...</p>
-      </main>
-    );
-  }
+  // if (!isSuccess) {
+  //   return (
+  //     <main className='w-screen h-screen flex justify-center items-center bg-gradient-to-r from-[#FAF7F0] to-[#F7D4C3]'>
+  //       <p className='text-4xl font-semibold text-[#e16b31]'>LOADING...</p>
+  //     </main>
+  //   );
+  // }
 
   return <main>{children}</main>;
 }
