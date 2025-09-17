05cfbf2b676c22f04f29c873a454173ab3c8180e
incepthink <incepthink@gmail.com>
2025-09-17 16:03:40 +0530

    feat: added analytics

--- src/app/liquidity/Layout.tsx
+++ src/app/liquidity/Layout.tsx
@@ -2,11 +2,17 @@
 
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export default function LiquidityLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <main>{children}</main>;
+  return (
+    <>
+      <main>{children}</main>
+      <GoogleAnalytics gaId="G-LEDP7GJQXL" />
+    </>
+  );
 }
